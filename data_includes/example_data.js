d0cdacc3fc1802faeb1791c919b502c5f7ac572f
Dylan Zuniga <dzu@sas.upenn.edu>
2018-04-15 16:14:26 -0400

    Fixed commas and added shuffling

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,4 +1,4 @@
-var shuffleSequence = seq("intro", "stop");
+var shuffleSequence = seq("intro", reshuffle("stop"));
 
 var defaults = [
     "Question", {
@@ -17,39 +17,39 @@ var items = [
     ["stop", "Message", {html: "<p>Daniela says, “People didn't stop looking at their phone " +
               "on the slide about the history of the justice system.”</p>", q: "Given what " +
               "your Daniela told you, do you think it's possible people were paying " +
-              "attention before the slide?"}]
+              "attention before the slide?"}],
     ["stop", "Message", {html: "<p>Tamara says, “People didn't stop yawning on the slide about " +
               "immigrant narratives.”</p>", q: "Given what your Tamara told you, do " +
-              "you think it's possible that people were yawning before the slide?"}]
+              "you think it's possible that people were yawning before the slide?"}],
     ["stop", "Message", {html: "<p>Ben says, “People didn't stop talking over you on " +
               "the beginning slide.”</p>",q: "Given what your Ben told you, do you think that it " +
-              "is possible people were talking over you before the slide?"}]
+              "is possible people were talking over you before the slide?"}],
     ["stop", "Message", {html: "<p>Zoe says, “People didn't stop falling asleep " +
               "on the slide about mental health.”</p>", q: "Given what your Zoe told you, " +
-              "do you think it's possible people were falling asleep before the slide?"}]
+              "do you think it's possible people were falling asleep before the slide?"}],
     ["stop", "Message", {html: "<p>Joe says, “People didn't stop walking out on the middle " + 
               "of the presentation.</p>", q: "Given what your Joe told you, do you think that it " +
-              "is possible people were walking out before the middle of the presentation?"}]
+              "is possible people were walking out before the middle of the presentation?"}],
     ["stop", "Message", {html: "<p>Katherine says, “People didn't stop smirking on the slide " +
               "with your call-to-action. ”</p>", q: "Given what your Katherine told you, do you think it's " +
-              "possible people smirking before the slide?"}]
+              "possible people smirking before the slide?"}],
     ["stop", "Message", {html: "<p>Mary says, “People didn't stop paying attention on the slide " +
               "with the information about non-profit organizations.”</p>", q: "Given what your Mary told you, " +
-              "do you think it's possible people were paying attention before the slide?"}]    
+              "do you think it's possible people were paying attention before the slide?"}],   
     ["stop", "Message", {html: "<p>Brianna says, “People didn't stop being motivated on the " +
               "slide with the retention rates.”</p>", q: "Given what your Brianna told you, " +
-              "do you think it's possible people were motivated before the slide?"}]  
+              "do you think it's possible people were motivated before the slide?"}], 
     ["stop", "Message", {html: "<p>Elise says, “People didn't stop engaging with you on " +
               "the slide with the big numbers.”</p>", q: "Given what your Elise told you, " +
-              "do you think it's possible people were engaging with you before the slide?"}]
+              "do you think it's possible people were engaging with you before the slide?"}],
     ["stop", "Message", {html: "<p>Michaela says, “People didn't stop nodding on the slide " + 
               "showcasing different stories about people affected.”</p>", q: "Given what " + 
               "your Michaela told you, do you think it's possible people were paying nodding " + 
-              "before the slide?"}]
+              "before the slide?"}],
     ["stop", "Message", {html: "<p>Dylan says, “People didn't stop asking good questions on " +
               "the middle of the presentation.”</p>", q: "Given what your Dylan told you, do " + 
               "you think it's possible people were asking good questions before the middle " + 
-              "of the presentation?"}]
+              "of the presentation?"}],
     ["stop", "Message", {html: "<p>Rachel says, “People didn't stop clapping on the slide " +
               "encouraging people to call their senators.”</p>", q: "Given what your Rachel " +
               "told you, do you think it's possible people were clapping before the slide?"}]
